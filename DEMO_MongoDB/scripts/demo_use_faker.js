a347b6fe2299dd86b4d24e75d48ab9e4bbacbe64
Mephi <182352808+Mephi78@users.noreply.github.com>
2025-03-02 15:52:46 +0100

    add faker demo, .gitignore, cleanup

--- DEMO_MongoDB/scripts/demo_use_faker.js
+++ DEMO_MongoDB/scripts/demo_use_faker.js
@@ -1,9 +1,40 @@
 // include modules
-const FAKER = require('faker');
+const { faker } = require('@faker-js/faker');
+// curly braces mean, you initialize an Object
 const { MongoClient } = require('mongodb');
 
-async function seedDatabase() {
+async function seedDb() {
+  // CONNECTION STRING >>>>> edit to your needs <<<<<
+  const URI = "mongodb://localhost:27017";
+  const CLIENT = new MongoClient(URI);
 
+  try {
+    await CLIENT.connect();
+    console.log('Connection established.');
+
+    const DB = CLIENT.db('FakerTest');    // use database FakerTest
+    const BOOKS = DB.collection('books');
+    //clear collection books before seeding
+    BOOKS.drop();
+
+    // create some fake books data
+    let bookList = [];
+
+    // get random book objects
+    for (let i = 0; i < 10; i++) {
+      let newBook = {
+        encounter: faker.date.past(),
+        author: faker.person.fullName(),
+        title: faker.book.title(),        // JS allows trailing commas in arrays, Objects, function parameters...
+      };
+      bookList.push(newBook);
+    }
+    await BOOKS.insertMany(bookList);
+    console.log('Books collection seeded.')
+  } finally {
+    await CLIENT.close();
+    console.log('Database connection closed.')
+  }
 }
 
-seedDatabase().catch(console.dir);
+seedDb().catch(console.dir);
--- DEMO_MongoDB/scripts/demo_use_mongodb.js
+++ DEMO_MongoDB/scripts/demo_use_mongodb.js
@@ -3,31 +3,30 @@ const { MongoClient } = require('mongodb');
 
 // asynchronous functions will run in background
 async function run() {
-    // CONNECTION STRING >>>>> edit to your needs <<<<<
-    const URI = "mongodb://localhost:27017";
-    const CLIENT = new MongoClient(URI);
+  // CONNECTION STRING >>>>> edit to your needs <<<<<
+  const URI = "mongodb://localhost:27017";
+  const CLIENT = new MongoClient(URI);
 
-    try {
-        await CLIENT.connect();
-        console.log('Connection opened.');
+  try {
+    await CLIENT.connect();
+    console.log('Connection established.');
 
-        await listDatabases(CLIENT);
-
-    } finally {
-        console.log('Closing connection...');
-        await CLIENT.close();
-        console.log('Connection closed.');
-    }
+    await listDatabases(CLIENT);
+  } finally {
+    console.log('Closing connection...');
+    await CLIENT.close();
+    console.log('Connection closed.');
+  }
 }
 
 // lists all databases (collections) in your MongoDb
 async function listDatabases(client) {
-    const DB_LIST = await client.db().admin().listDatabases();
-    console.log('Listing databases...');
-    DB_LIST.databases.forEach(db => {
-        // literal template
-        console.log(` * ${db.name}`);
-    });
+  const DB_LIST = await client.db().admin().listDatabases();
+  console.log('Listing databases...');
+  DB_LIST.databases.forEach(db => {
+    // literal template
+    console.log(` * ${db.name}`);
+  });
 }
 
 run().catch(console.dir);
